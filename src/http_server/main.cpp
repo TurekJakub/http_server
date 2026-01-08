ee2a2558ac4cfbf6a025efb178f4b2816543f8bd
TurekJakub <jakubturek32@gmail.com>
2026-01-08 23:01:36 +0100

    add simple demo example

--- src/http_server/main.cpp
+++ src/http_server/main.cpp
@@ -1,3 +1,83 @@
+/*
+    This example doesn't have much in common with scope
+    of this project, except of used technologies. Therfore
+    this simple demo, that doesn't do much more then basically
+    pinging Arch Linux servers, serves just as demonstration that
+    project can be automaticaly build and linked with all it dependencies
+    (Asio, OpenSSL), which can then use to perform basic network
+    communication with TLS support
+*/
+
+#include <format>
+#include <print>
+#include <string>
+
+#include "asio/completion_condition.hpp"
+#include "asio/error.hpp"
+#include "asio/error_code.hpp"
+#include "asio/ssl/verify_mode.hpp"
+#include "asio/system_error.hpp"
+#include <asio.hpp>
+#include <asio/ssl.hpp>
+
+using namespace std;
+using namespace asio;
+
 int main() {
-    return 0;
+  const string host = "www.archlinux.org";
+  const string target = "/";
+  const std::string port = "443";
+
+  try {
+    io_context io_context;
+
+    ssl::context ssl_context(ssl::context::tls_client);
+    // Using system trusted certs does not reliably on all platforms,
+    // so for purposes of this simple demo add root CA cert manually
+    ssl_context.load_verify_file("../src/resources/archlinux-org.pem");
+    ssl_context.set_verify_mode(ssl::verify_peer);
+
+    ip::tcp::resolver resolver(io_context);
+    ip::tcp::socket socket(io_context);
+
+    ssl::stream<ip::tcp::socket> ssl_stream(std::move(socket), ssl_context);
+    if (!SSL_set_tlsext_host_name(ssl_stream.native_handle(), host.c_str())) {
+      print("Failed to set SNI hostname.\n");
+      return 1;
+    }
+
+    auto endpoints = resolver.resolve(host, port);
+    print("Resolving {}...\n", host);
+
+    connect(ssl_stream.lowest_layer(), endpoints);
+    print("Connection established.\n");
+
+    ssl_stream.handshake(ssl::stream_base::client);
+    print("SSL handshake successful.");
+
+    string request = format("GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n", target, host);
+
+    write(ssl_stream, buffer(request));
+    print("HTTP request sent.\n");
+
+    asio::streambuf buffer;
+    error_code err;
+    while (true) {
+      asio::read(ssl_stream, buffer, transfer_at_least(1), err);
+      if (err == asio::error::eof) {
+        break;
+      } else if (err) {
+        print("Error while reading server response: {}\n", system_error(err).what());
+        return 1;
+      }
+    }
+
+    string response = string(buffers_begin(buffer.data()), buffers_end(buffer.data()));
+    print("Server response:\n\n{}\n", response);
+  } catch (exception &err) {
+    print("Error: {}\n", err.what());
+    return 1;
+  }
+
+  return 0;
 }
